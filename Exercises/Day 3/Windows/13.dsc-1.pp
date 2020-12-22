8794f3a270eef1d7bb22fd975df4ec350c0848db
Atin Gupta <atingupta2005@gmail.com>
2020-12-22 08:34:59 +0530

    -

--- Exercises/Day 3/Windows/13.dsc-1.pp
+++ Exercises/Day 3/Windows/13.dsc-1.pp
@@ -1,7 +1,7 @@
 #Installing Packages with DSC
 dsc_package{'installpython'
   dsc_ensure    => 'Present',
-  dsc_name      => 'Python 2.7.10',
-  dsc_productid => 'E2B51919-207A-43EB-AE78-733F9C6797C2'
-  dsc_path      => 'C:\\python.msi',
+  dsc_name      => 'Python 2.7.2',
+  dsc_productid => '2E295B5B-1AD4-4D36-97C2-A316084722CF'
+  dsc_path      => 'C:\Users\atingupta2005\Downloads\python-2.7.2.msi',
 }
